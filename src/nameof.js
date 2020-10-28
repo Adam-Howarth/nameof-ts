64b31c46a63beea92489a25d577ea8ecf8c4ba4f
Adam Howarth <ahowarth@starrez.com>
2020-10-28 21:07:13 +0000

    Updated js

--- src/nameof.js
+++ src/nameof.js
@@ -18,6 +18,20 @@ function nameof(nameFunction, options) {
     if (fnStr.includes("=>")) {
         return cleanseAssertionOperators(fnStr.substring((options === null || options === void 0 ? void 0 : options.lastProp) ? fnStr.lastIndexOf(".") + 1 : fnStr.indexOf(".") + 1));
     }
+    // ES5 prop selector:
+    // "function (x) { return x.prop; }"
+    var matchRegex = /function\s*\(\w+\)\s*\{[\r\n\s]*return\s+\w+\.((\w+\.)*(\w+))/i;
+    var es5Match = fnStr.match(matchRegex);
+    if (es5Match) {
+        return (options && options.lastProp)
+            ? es5Match[3]
+            : es5Match[1];
+    }
+    // ES5 class name:
+    // "function ClassName() { ..."
+    if (fnStr.startsWith("function ")) {
+        return cleanseAssertionOperators(fnStr.substring("function ".length, fnStr.indexOf("(")));
+    }
     // Invalid function.
     throw new Error("nameof-ts: Invalid function.");
 }
